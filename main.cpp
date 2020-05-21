ef76602d087f9d7d5ddfabebf03e7c8985f3f2f1
Vladimir <64377415+VovaMath@users.noreply.github.com>
2020-05-21 11:36:37 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -33,6 +33,10 @@ typedef struct link {
   } list;
 
 void L_print(list *L) { // вывод на экран
+    if(L==NULL) {
+        printf("List is empty\n");
+        return;
+    }
     printf("List:[");
     for(;;) {
         printf("%d ", L->id);
@@ -41,37 +45,69 @@ void L_print(list *L) { // вывод на экран
     }
     printf("]\n");
 }
-void L_start(list *L, int v) { // L:=список из одного элемента v
-    L->id = v;
-    L->next = NULL;
-}
-void L_pop_front(list *L, int v) { // вставить число v в начало списка L
+
+void L_pop_front(list *&L, int v) { // вставить число v в начало списка L
+    if( L==NULL) {
+        L = new link;
+        L->id = v;
+        L->next = NULL;
+        return;
+    }
     link *L1 = new link;
-    L1->id = L->id;
-    L1->next = L->next;
-    L->id = v;
-    L->next = L1;
+    L1->id = v;
+    L1->next = L;
+    L = L1;
 }
-void L_pop(list *L, int i, int v) { // вставить число v перед номером i в списке L
+
+void L_pop(list *&L, int i, int v) { // вставить число v перед номером i в списке L
+    if( L==NULL) {
+        L = new link;
+        L->id = v;
+        L->next = NULL;
+        return;
+    }
+    if(i==0) {
+        L_pop_front(L, v);
+        return;
+    }
+    link *L0=L, *L1;
     for( int j=0; j<i; j++) {
-        if( (L = L->next)==NULL) return;  // ошибка
+        L1 = L0;
+        if( (L0 = L0->next)==NULL) break;  // дошли до конца
     }
-    L_pop_front(L, v);
+    L_pop_front(L0, v);
+    L1->next = L0;
 }
-void L_popA(list *L, int i, int v) { // вставить число v ПОСЛЕ номера i в списке L
+void L_popA(list *&L, int i, int v) { // вставить число v ПОСЛЕ номера i в списке L
+    if( L==NULL) {
+        L = new link;
+        L->id = v;
+        L->next = NULL;
+        return;
+    }
+    link *L0=L, *L1;
     for( int j=0; j<=i; j++) {
-        if( (L = L->next)==NULL) return;  // ошибка
+        L1 = L0;
+        if( (L0 = L0->next)==NULL) break;  // дошли до конца
     }
-    L_pop_front(L, v);
+    L_pop_front(L0, v);
+    L1->next = L0;
 }
-void L_popE(list *L, int v) { // вставить число v в хвост списка L
-    while( L->next != NULL) L = L->next;
+void L_popE(list *&L, int v) { // вставить число v в хвост списка L
+    if( L==NULL) {
+        L = new link;
+        L->id = v;
+        L->next = NULL;
+        return;
+    }
+    list *L0 = L;
+    while( L0->next != NULL) L0 = L0->next;
     link *L1 = new link;
-    L->next = L1;
+    L0->next = L1;
     L1->id = v;
     L1->next = NULL;
 }
-void L_del1(list *L) { // удалить первый элемент
+void L_del1(list *&L) { // удалить первый элемент
     link *L1 = L->next;
     L->id = L1->id;
     L->next = L1->next;
@@ -84,45 +120,33 @@ void L_del(list *L, int i) { // удалить i-й элемент
     L_del1(L);
 }
 // Индивидуальное задание: из списка L построить списки M1 и M2 из отриц. и полож. элементов
-void Indi(list *L,list *M1,list *M2) {
+void Indi(list *L,list *&M1,list *&M2) {
     int x;
-    bool fM1=true, fM2=true;    // списки пустые?
-    while( L->next != NULL)
+    M1 = M2 = NULL;
+    while( L != NULL)
     {
         x=L->id;
-        if (x>0 )
-        {
-            if( fM1) {
-                L_start(M1, x);
-                fM1=false;
-            } else
-            L_popE(M1,x);
-        }
-        if ((x )<0 )
-        {
-            if( fM2) {
-                L_start(M2,x);
-                fM2 = false;
-            } else
-            L_popE(M2,x);
-        }
+        if (x>0 ) L_popE(M1,x);
+        if (x<0 ) L_popE(M2,x);
         L = L->next;
     }
 }
 //------------------------------------------------------
 int main()
 {
-    list L, M1, M2;
-    int k, r;
-    /*
-    L_start(&L, 11);
-    L_popE(&L, -22);
-    L_popE(&L, 33);
-    L_popE(&L, 44);
-    L_popE(&L, -55);
-    L_popE(&L, 66);
-    */
+    list *L=NULL, *M1=NULL, *M2=NULL;
 
+    for(int i=0;i<10;i++){
+
+        L_pop_front(L, rand()%10-5);
+    }
+
+    L_print(L);
+
+    /*
+    int k, r;
+    for(int i=0; i<10; i++)
+        printf("%d\n", rand()%100);
 
     bool first = true;
     do {
@@ -139,7 +163,8 @@ int main()
     } while(k != 0);
 
     L_print(&L);
-    int action;
+    */
+    int k, r, action;
     do {
         printf("\n0: Exit\n"
                "1: Insert at the begin\n"
@@ -154,15 +179,15 @@ int main()
         if(action==1) {
             printf("Enter number");
             scanf("%d",&k);
-            L_pop_front(&L, k);
-            L_print(&L);
+            L_pop_front(L, k);
+            L_print(L);
             continue;
         }
         if(action==2) {
             printf("Enter number");
             scanf("%d",&k);
-            L_popE(&L, k);
-            L_print(&L);
+            L_popE(L, k);
+            L_print(L);
             continue;
         }
         if(action==3) {
@@ -170,8 +195,8 @@ int main()
             scanf("%d",&r);
             printf("Enter number");
             scanf("%d",&k);
-            L_pop(&L, r, k);
-            L_print(&L);
+            L_pop(L, r, k);
+            L_print(L);
             continue;
         }
         if(action==4) {
@@ -179,32 +204,31 @@ int main()
             scanf("%d",&r);
             printf("Enter number");
             scanf("%d",&k);
-            L_pop(&L, r+1, k);
-            L_print(&L);
+            L_pop(L, r+1, k);
+            L_print(L);
             continue;
         }
         if(action==5) {
-            L_del1(&L);
-            L_print(&L);
+            L_del1(L);
+            L_print(L);
             continue;
         }
         if(action==6) {
             printf("Enter index");
             scanf("%d",&r);
-            L_del(&L, r);
-            L_print(&L);
+            L_del(L, r);
+            L_print(L);
             continue;
         }
         if(action==7) {
-            printf("L :"); L_print(&L);
-            Indi(&L, &M1, &M2);
-            printf(">0:"); L_print(&M1);
-            printf("<0:"); L_print(&M2);
+            printf("L :"); L_print(L);
+            Indi(L, M1, M2);
+            printf(">0:"); L_print(M1);
+            printf("<0:"); L_print(M2);
             continue;
         }
 
     } while(1);
-
 
 return 0;
 }
